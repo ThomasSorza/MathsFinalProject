1d4dc1cc8a3a741d69104089d2b916cea9a0aad7
Esteban Barrero <barrerodavid10@gmail.com>
2023-05-29 22:24:26 -0500

    Creación de menu principal

--- src/View.py
+++ src/View.py
@@ -1,8 +1,5 @@
-#Menú_Proyecto
-
-#--------------Menú----------------
-
 from Directed_Graph import Directed_Graph
+from Default_values import Default_values
 
 class View:
     def __init__(self):
@@ -11,13 +8,45 @@ class View:
     def display_graph(self):
         self.graph.show_graph()
 
+   # def 
 
 class ViewMenu:
 
     def menuPrincipal():
-        print("*** Bienvenido a nuestro Menú Principal *** \n1. Iniciar con valores preterminados\n2. Ingresar valores\n3. Mostrar Grafo\n\n0. Salir\n")
+        while True:
+            print("*** Bienvenido a nuestro Menú Principal *** \n1. Iniciar con valores preterminados\n2. Ingresar valores\n3. Mostrar Grafo\n\n0. Salir\n")
+            opcion = input("Ingrese una opción: ")
 
-    def menuTransferencia():
+            if opcion == "1":
+                print("Hola1")
+                menuTarjeta()
+            elif opcion == "2":
+                print("Ha seleccionado la Opción 2")
+                # Aquí puedes colocar el código correspondiente a la opción 2
+        
+            elif opcion == "3":
+                print("Ha seleccionado la Opción 3")
+                # Crear una instancia de la clase View
+                v = View()
+                v.display_graph()
+            
+                print("No sucede nada")
+    
+            elif opcion == "0":
+                print("Todo bien")
+                break
+            else:
+                print("Opción inválida. Por favor, ingrese una opción válida.")
+    
+    if __name__ == "__main__":
+        print("HolaMundo")
+        menuPrincipal()
+
+
+
+
+
+    """def menuTransferencia():
         #print("Por favor ingrese el numero de cuenta")
         opcion = input("Ingrese el tipo de cuenta:\n1. Ahorros\n2. corriente\n0. Salir")
         opcionNumeroCuenta = ""
@@ -26,14 +55,10 @@ class ViewMenu:
         #if opcionNumeroCuenta !=0:
 
         #elif opcion == "2":
-            #print("")
+            #print("")"""
 
 
-    def menuTipoCuenta():
-        print
-
-
-    def menuTarjeta():
+    """def menuTarjeta():
         optionTarjeta = ""
         while True:
             print("** Menú Tarjeta**\n1. Trasferencia\n2. Retirar\n3. Ingreso Cuenta\n4. Consultar saldo\n0. Salir\n")
@@ -42,36 +67,9 @@ class ViewMenu:
                 menuTransferencia()
             elif optionTarjeta == "2":
                 print("La buena")
-            break
+            break"""
 
 
 
   
-    while True:
-        menuPrincipal()
-        opcion = input("Ingrese una opción: ")
-
-        if opcion == "1":
-            print("Hola1")
-            menuTarjeta()
-        
-        
-
-
-        elif opcion == "2":
-            print("Ha seleccionado la Opción 2")
-            # Aquí puedes colocar el código correspondiente a la opción 2
-        
-        elif opcion == "3":
-            print("Ha seleccionado la Opción 3")
-            # Crear una instancia de la clase View
-            v = View()
-            v.display_graph()
-            
-            print("No sucede nada")
     
-        elif opcion == "0":
-            print("Todo bien")
-            break
-        else:
-            print("Opción inválida. Por favor, ingrese una opción válida.")
